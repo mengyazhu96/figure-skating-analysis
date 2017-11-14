2d4718e09fe5f845cbc9d5b7ad4aaaab0e658700
Jessica Zhu <mengyazhu96@gmail.com>
2017-11-14 10:44:06 -0500

    Refactor isu.py

--- code/data/isu.py
+++ code/data/isu.py
@@ -1,38 +1,54 @@
+#!/usr/bin/env python
+
 from lxml import html, etree
 import requests
-# import sys
+import sys
 
-# seasons = ['season1516', 'season1617']
-seasons = ['season1314', 'season1415']
-gps = ['usa', 'can', 'chn', 'rus', 'fra', 'jpn']
-gps = ['gp' + gp for gp in gps]
-# gp_years = ['2015', '2016']
-gp_years = ['2009', '2010']
-champs = ['ec', 'fc', 'wc']
-champ_years = [str(int(year) + 1) for year in gp_years]
-gpf_years = ['0809', '0910']
 
-all_events = []
-for s, season in enumerate(seasons):
-	events = [gp + gp_years[s] for gp in gps]
-	events.append('gpf' + gpf_years[s])
-	events += [champ + champ_years[s] for champ in champs]
-	events = map(lambda event_name: event_name, events)
-	# events = map(lambda event_name: event_name + '/' + event_name + '_Men_SP_Scores.pdf', events)
-	all_events += events
+def get_events(seasons):
 
-for event in all_events:
-	page = requests.get('http://www.isuresults.com/results/' + event)
-	# print len(page.content), 
-	# if len(page.content) < 10:
-	# 	print event
-	try:
-		tree = html.fromstring(page.content)
-	except etree.ParserError:
-		print 'Event not found: % s' % event
-		continue
+    # i.e. 10
+    two_digit_seasons = [int(season[2:]) for season in seasons]
 
-	if not list(tree.iter('title')):
-		print 'Event not found: % s' % event
-	else:
-		print list(tree.iter('title'))[0].text
+    # i.e. 0910
+    two_to_two_seasons = [str(season - 1) + str(season) for season in two_digit_seasons]
+
+    season_urls = ['season' + season for season in two_to_two_seasons]
+    gp_years = [str(int(season) - 1) for season in seasons]
+    champ_years = seasons
+    gpf_years = two_to_two_seasons
+
+    champs = ['ec', 'fc', 'wc']
+    gps = ['usa', 'can', 'chn', 'rus', 'fra', 'jpn']
+    gps = ['gp' + gp for gp in gps]
+
+    all_events = []
+    for s, season in enumerate(season_urls):  # season_url only used for 15-16, 16-17
+        events = [gp + gp_years[s] for gp in gps]
+        events.append('gpf' + gpf_years[s])
+        events += [champ + champ_years[s] for champ in champs]
+        events = map(lambda event_name: event_name, events)
+        # events = map(lambda event_name: event_name + '/' + event_name + '_Men_SP_Scores.pdf', events)
+        all_events += events
+    return all_events
+
+def print_events(events):
+    for event in events:
+        page = requests.get('http://www.isuresults.com/results/' + event)
+        try:
+            tree = html.fromstring(page.content)
+        except etree.ParserError:
+            print 'Event not found: % s' % event
+            continue
+
+        if not list(tree.iter('title')):
+            print 'Event not found: % s' % event
+        else:
+            print list(tree.iter('title'))[0].text
+
+if __name__ == '__main__':
+    seasons = ['2014', '2015']
+    if len(sys.argv) > 1:
+        seasons = sys.argv[1:]
+    events = get_events(seasons)
+    print_events(events)
